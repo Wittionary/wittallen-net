12784e149afe302dd68e9a7374bde3c19b092776
Witt Allen <wittionary@users.noreply.github.com>
2023-07-28 00:42:22 -0500

    Rewriting to support TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: [
+    '_src/*.html',
+    '_src/**/*.html',
+    '_layouts/*.njk',
+  ],
+}
+
